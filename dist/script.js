58af73792c885f689ab6607250fd74e0b1ce4826
Alan Bubalo <alan.buba5@gmail.com>
2022-02-17 00:30:47 +0100

    Updated send button

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,8 @@
+/*
 (function () {
   emailjs.init("user_K5XZXYFal24z6Fqcdc9uO");
 })();
-
+*/
 $(document).ready(function () {
   $(".animate").hover(
     function () {
@@ -24,14 +25,29 @@ window.onload = function () {
     .getElementById("contact-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
-
+      let send = document.getElementById("send");
+      let sending = document.getElementById("sending");
+      let not_sending = document.getElementById("not_sending");
+      // document.getElementByID("send").classList.add("")
+      send.disabled = true;
+      send.classList.add("sending");
+      sending.classList.remove("hidden");
+      not_sending.classList.add("hidden");
       emailjs.sendForm("service_waevsdx", "template_y2dl8ob", this).then(
         () => {
           alert("SUCCESS!");
+          send.disabled = false;
+          send.classList.remove("sending");
+          sending.classList.add("hidden");
+          not_sending.classList.remove("hidden");
         },
         (err) => {
           console.log(err);
           alert("FAILED...");
+          send.disabled = false;
+          send.classList.remove("sending");
+          sending.classList.add("hidden");
+          not_sending.classList.remove("hidden");
         }
       );
     });
